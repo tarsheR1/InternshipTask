ae2dd3ed9c9535b939c47a05dda16f6ba880b02b
Никита Мацкевич <matskevich_1998@bk.ru>
2025-03-11 20:13:09 +0300

    Добавьте файлы проекта.

---
+++ Microservices/WebApplication1/Domain/Interfaces/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using EventManagementService.Domain.Models;
+
+namespace EventManagementService.Domain.Interfaces
+{
+    public interface IEventRepository 
+    {
+        Task AddAsync(EventEntity eventEntity);
+        Task<EventEntity> GetEventAsync(Guid eventId);
+        Task<List<EventEntity>> GetAllAsync();
+        Task UpdateAsync(EventEntity eventEntity);
+        Task DeleteAsync(Guid eventId);
+
+        Task<(List<EventEntity> Events, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize);
+    }
+}
