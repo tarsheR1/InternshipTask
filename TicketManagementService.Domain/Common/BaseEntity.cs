e0b1d2bf221c78718f23e69e3ef6758e546368fb
Никита Мацкевич <matskevich_1998@bk.ru>
2025-04-01 16:20:19 +0300

    Infrastructure Layer, Domain Layer

--- TicketManagementService.Domain/Common/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TicketManagementService.Domain.Common
-{
-    public abstract class BaseEntity
-    {
-        public string Id { get; protected init; } = Guid.NewGuid().ToString();
-
-    }
-}
