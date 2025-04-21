ad2ce3e581b45307ff2c20dcea5b2fc831ca31ab
Никита Мацкевич <matskevich_1998@bk.ru>
2025-04-21 22:29:07 +0300

    Remove data annotations

--- UserManagementService.PresentationLayer/DTO/Request/LoginRequestDto.cs
+++ UserManagementService.PresentationLayer/DTO/Request/LoginRequestDto.cs
@@ -1,9 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace UserManagementService.PresentationLayer.DTO.Request
+﻿namespace UserManagementService.PresentationLayer.DTO.Request
 {
     public record LoginRequestDto(
-        [Required][EmailAddress] string Email,
-        [Required][MinLength(6)] string Password
+        string Email,
+        string Password
     );  
 }
