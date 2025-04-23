9283a3f5fae43399d2610d99dc9f4309c6a7ce47
Никита Мацкевич <matskevich_1998@bk.ru>
2025-04-24 00:59:24 +0300

    pagination

---
+++ EventManagementService.Domain/Models/Pagination/PaginationOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace EventManagementService.Domain.Models.Pagination
+{
+    public class PaginationOptions
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
