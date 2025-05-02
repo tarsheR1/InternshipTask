1c634439f46cbca899f8abd096516500be8ed4e0
Никита Мацкевич <matskevich_1998@bk.ru>
2025-05-02 10:34:18 +0300

    Authen and author policies

--- TicketManagementService.API/Program.cs
+++ TicketManagementService.API/Program.cs
@@ -1,12 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
-// Configure the HTTP request pipeline.
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApproveEvent", policy =>
+        policy.RequireClaim("permission", "ApproveEvent"));
+
+    options.AddPolicy("ModerateEvents", policy =>
+        policy.RequireClaim("permission", "ModerateEvents"));
+});
 
 app.UseHttpsRedirection();
 
